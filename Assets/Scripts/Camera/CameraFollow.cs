59c455eb490b16b8916e31d81467d4b06a61864e
NorthTH <North@Damris-MacBook-Pro.local>
2016-08-28 14:35:25 +0700

    First SurvivorShooter

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform target;
+	public float smooting;
+
+	Vector3 offset;
+
+	void Start()
+	{
+		offset = transform.position - target.position;
+	}
+
+	void FixedUpdate()
+	{
+		Vector3 targetCampos = target.position + offset;
+		transform.position = Vector3.Lerp (transform.position, targetCampos, smooting * Time.deltaTime);
+	}
+}
